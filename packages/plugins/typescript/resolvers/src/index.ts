19fc2955bbf0cc2647ea4446114d0b9200a95cef
Laurin Quast <laurinquast@googlemail.com>
2021-07-17 15:41:56 +0200

    remove unused import

--- packages/plugins/typescript/resolvers/src/index.ts
+++ packages/plugins/typescript/resolvers/src/index.ts
@@ -1,7 +1,7 @@
 import { printSchemaWithDirectives } from '@graphql-tools/utils';
 import { parseMapper } from '@graphql-codegen/visitor-plugin-common';
 import { Types, PluginFunction, addFederationReferencesToSchema } from '@graphql-codegen/plugin-helpers';
-import { parse, visit, GraphQLSchema, printSchema } from 'graphql';
+import { parse, visit, GraphQLSchema } from 'graphql';
 import { TypeScriptResolversVisitor } from './visitor';
 import { TypeScriptResolversPluginConfig } from './config';
 
